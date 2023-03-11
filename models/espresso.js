abab39079c3249c174357ed542ab97c8836b324a
garyzhuo <gary_zhuo@hotmail.com>
2023-03-11 15:35:48 -0600

    Added the required folders, filled the necessary requirements for the server.js

---
+++ models/espresso.js
@@ -0,0 +1,24 @@
+// Require the Mongoose package
+const mongoose = require('mongoose');
+
+// This is my first schema, this would be used for my espresso coffee
+const espressoSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  photo: String,
+  price: String,
+  description: { type: String, required: true},
+  roast: {type: String, requried: true},
+  isFeatured: { type: Boolean, default: false}
+});
+
+
+/*const reviewsSchema = mongoose.Schema({
+    name: String,
+    review: String,
+    reviewDate: String
+}); */
+
+// Export the schema as a Monogoose model. 
+// The Mongoose model will be accessed in `models/index.js`
+//Will be grabbing the file from models/index
+module.exports = mongoose.model('Espresso', espressoSchema);
---
+++ models/seed.js
@@ -0,0 +1,9 @@
+const espressos = [
+    {
+        name: 'Illy Classico Espresso Ground Coffee',
+    }
+]
+
+
+// this will export the seed data to models/index.js 
+module.exports = espressos
